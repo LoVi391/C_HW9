41cd0ab04e0bb7967079cfb24d320784328a7278
Lola <lolitasav884@gmail.com>
2022-06-07 00:08:24 +0300

    66

---
+++ 66/Program.cs
@@ -0,0 +1,14 @@
+﻿// // // Задайте значения M и N. Напишите программу, которая найдёт
+// сумму натуральных элементов в промежутке от M до N.
+
+int m = 1;
+int n = 15;
+int result = 0;
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {m} до {n} равна {Recursion(m, n)}.");
+
+int Recursion(int firstNumber, int lastNumber)
+{
+    if (firstNumber == lastNumber + 1) return result;
+    result += firstNumber;
+    return Recursion(firstNumber + 1, lastNumber);
+}
